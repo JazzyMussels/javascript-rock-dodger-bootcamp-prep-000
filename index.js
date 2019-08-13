e871aaa5357c28167585e1b5fb5965084e165f58
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-13 00:13:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -100,6 +100,7 @@ function createRock(x) {
   // We should kick of the animation of the rock around here
   if (top < GAME_HEIGHT) {
 window.requestAnimationFrame(moveRock)
+} else 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
